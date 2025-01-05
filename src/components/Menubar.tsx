f493c4dd8cafbd5ed13f5d3ed9cd6913025f726b
Shakil Ahmed <shakilahmmeed@gmail.com>
2025-01-05 14:38:12 +0600

    Update Menubar component styles and adjust icon sizes

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -4,8 +4,8 @@ import WifiIcon from "assets/img/menu/wifi.png";
 
 export default function Menubar() {
 	return (
-		<div className="bg-white bg-opacity-10 w-full h-[28px] absolute top-0 flex items-center justify-between px-4">
-			<div className="flex items-center text-sm">
+		<div className="bg-white bg-opacity-10 w-full h-[28px] absolute top-0 flex items-center justify-between px-4 text-[13px]">
+			<div className="flex items-center">
 				<h3 className="font-semibold">Finder</h3>
 				<ul className="flex items-center gap-3.5 ml-6">
 					<li>File</li>
@@ -16,10 +16,14 @@ export default function Menubar() {
 					<li>Help</li>
 				</ul>
 			</div>
-			<div className="flex items-center gap-3 text-sm">
-				<img src={ControlCenterIcon} alt="control-center" />
-				<img src={SearchIcon} alt="search" />
-				<img src={WifiIcon} alt="wifi" />
+			<div className="flex items-center gap-4">
+				<img src={WifiIcon} alt="wifi" className="w-[16px]" />
+				<img src={SearchIcon} alt="search" className="w-[14px]" />
+				<img
+					src={ControlCenterIcon}
+					alt="control-center"
+					className="w-[14px]"
+				/>
 				<div className="pl-1">Sun 5 Jan 2:27 PM</div>
 			</div>
 		</div>
